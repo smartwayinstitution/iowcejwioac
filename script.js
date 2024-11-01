355e7127558bc3c8c74ffc66a0dfa1a9fd061aa9
smartwayinstitution <smartway@smartwayinstitution.com>
2024-11-01 21:19:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// إظهار/إخفاء نموذج تسجيل الدخول عند النقر على زر تسجيل الدخول
+let loginform = document.querySelector('.login-form');
+document.querySelector('#login-btn').onclick = () => {
+    loginform.classList.toggle('active');
+}
+
+// إظهار/إخفاء شريط التنقل عند النقر على زر القائمة
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active'); // إظهار وإخفاء القائمة
+    loginform.classList.remove('active'); // إغلاق أي نافذة تسجيل دخول مفتوحة
+}
+
+// تفعيل مكتبة lightGallery لمعرض الصور
+document.addEventListener("DOMContentLoaded", function() {
+    lightGallery(document.querySelector('.gallery .gallery-container'));
+});
